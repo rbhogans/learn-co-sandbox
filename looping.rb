9f1665c6fba67875b13e77aa46faa79aa33d5796
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-15 05:25:32 +0000

    Automatically backed up by Learn

---
+++ looping.rb
@@ -0,0 +1,17 @@
+toppings = ["pickles", "mushrooms", "bacon"]
+ 
+def hamburger(toppings)
+  toppings.each do |topping|
+    puts "I love #{topping} on my burgers!"
+  end
+end
+
+
+
+
+
+
+
+
+
+#def looping   puts"Never pass a school bus if it has flashing red lights" end   looping
